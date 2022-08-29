c7089558eeea068b5489c5029df867d772983b0c
Biswajit Sahoo <biswa4u85@gmail.com>
2022-08-29 16:06:17 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Erp Cms Management
+
+Erp Cms Management APP
+
+#### License
+
+MIT
---
+++ erp_cmsmanagement/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Erp Cms Management",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Erp Cms Management")
+		}
+	]
